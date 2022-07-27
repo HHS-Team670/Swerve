e8f2e452e6421f4ff0c81bf6bb4cfe18bc2c6f13
DevTeamHRT <dev@homesteadrobotics.com>
2022-07-26 21:02:27 -0700

    madmax is driving me mad
    :C

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,23 +28,29 @@ public final class Constants {
 
     public static final double LIMIT = 0.05;
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 21; //  Set front left module drive motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 20; //  Set front left module steer motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 30; //  Set front left steer encoder ID
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(74.79); //   Measure and set front left steer offset
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 21; //  Set front left module drive motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 20; //  Set front left module steer motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 30; //  Set front left steer encoder ID
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(-74.79); //   Measure and set front left steer offset
+    //public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0); //   Measure and set front left steer offset
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 23; //  Set front right drive motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 22; //  Set front right steer motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 32; //  Set front right steer encoder ID
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(31.7285); //  Measure and set front right steer offset
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 23; //  Set front right drive motor ID
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 22; //  Set front right steer motor ID
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 32; //  Set front right steer encoder ID
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(-31.7285); //  Measure and set front right steer offset
+    //public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0); //  Measure and set front right steer offset
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 27; //  Set back left drive motor ID
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 26; //  Set back left steer motor ID
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 36; //  Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(320.3613); //  Measure and set back left steer offset
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 27; //  Set back left drive motor ID
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 26; //  Set back left steer motor ID
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 36; //  Set back left steer encoder ID
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(320.3613); //  Measure and set back left steer offset
+    //public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0); //  Measure and set back left steer offset
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 25; //  Set back right drive motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 24; //  Set back right steer motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 34; //  Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(292.9394); //  Measure and set back right steer offset
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 25; //  Set back right drive motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 24; //  Set back right steer motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 34; //  Set back right steer encoder ID
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(292.9394); //  Measure and set back right steer offset
+    //public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0); //  Measure and set back right steer offset
+
 }
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import static frc.robot.Constants.*;
 
@@ -171,17 +172,19 @@ public class DrivetrainSubsystem extends SubsystemBase {
   }
 
   public Rotation2d getGyroscopeRotation() {
+    return Rotation2d.fromDegrees(0);
+
     // FIXME Remove if you are using a Pigeon
     // return Rotation2d.fromDegrees(m_pigeon.getFusedHeading());
 
     // FIXME Uncomment if you are using a NavX
-    if (m_navx.isMagnetometerCalibrated()) {
-//      // We will only get valid fused headings if the magnetometer is calibrated
-     return Rotation2d.fromDegrees(m_navx.getFusedHeading());
-    }
-//
-//    // We have to invert the angle of the NavX so that rotating the robot counter-clockwise makes the angle increase.
-    return Rotation2d.fromDegrees(360.0 - m_navx.getYaw());
+//     if (m_navx.isMagnetometerCalibrated()) {
+// //      // We will only get valid fused headings if the magnetometer is calibrated
+//      return Rotation2d.fromDegrees(m_navx.getFusedHeading());
+//     }
+// //
+// //    // We have to invert the angle of the NavX so that rotating the robot counter-clockwise makes the angle increase.
+//     return Rotation2d.fromDegrees(360.0 - m_navx.getYaw());
   }
 
   public void drive(ChassisSpeeds chassisSpeeds) {
@@ -191,11 +194,25 @@ public class DrivetrainSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(m_chassisSpeeds);
+    //Testing purpose
+    for(int i = 0; i < 4; i++) {
+        SmartDashboard.putNumber("module # " + i + " speed", states[i].speedMetersPerSecond);
+        SmartDashboard.putNumber("module # " + i + " angle", states[i].angle.getRadians());
+    }
+    //tab.getLayout("Chassis speeds", BuiltInLayouts.kList)
+    
     SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
+        
+    //VALID ^^^^
 
-    m_frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
-    m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
-    m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
-    m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
-  }
+//     m_frontLeftModule.set(  states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
+//     m_frontRightModule.set( states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
+//     m_backLeftModule.set(   states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
+//     m_backRightModule.set(  states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+        m_frontLeftModule.set(  states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
+        m_frontRightModule.set( states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
+        m_backLeftModule.set(   states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
+        m_backRightModule.set(  states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+
+   }
 }
